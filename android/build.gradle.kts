8affeffcc49373ffbdc472421db22298c4e323f1
qhxk1ng <abrahambennyy@gmail.com>
2026-02-20 23:34:30 +0530

    Remove mapbox_gl package - use flutter_map only, clean up CI and Gradle

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,16 +2,6 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven {
-            url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
-            authentication {
-                create<BasicAuthentication>("basic")
-            }
-            credentials {
-                username = "mapbox"
-                password = providers.gradleProperty("MAPBOX_DOWNLOADS_TOKEN").orElse("").get()
-            }
-        }
     }
 }
 
